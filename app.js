6703bb724a0caf1587013d711e16e546d16f1462
manvisingh3097 <singh.thakurmanvi@gmail.com>
2023-09-26 01:08:56 +0530

    modified

--- app.js
+++ app.js
@@ -6,46 +6,49 @@
 
 // console.log('fetching data')
 
-let api = "http://www.omdbapi.com/?apikey=83fd7607&=dangal";
+let api = "https://www.omdbapi.com/?apikey=83fd7607&t= ";
 
-fetch(api).then((data)=>{
-    return data.json()
-}).then((data)=>{
-    console.log(data)
-})
+// fetch("https://www.omdbapi.com/?apikey=83fd7607&t=").then((data)=>{
+//     return data.json()
+// }).then((data)=>{
+//    console.log(data)
+// })
 
-let title = document.getElementById('title')
-let data = document.getElementById('data')
-let plot = document.getElementById('plot')
-let actor = document.getElementById('actor')
-let director = document.getElementById('director')
-let genre = document.getElementById('genre')
-let collection = document.getElementById('collection')
-let poster = document.getElementById('poster')
-let award = document.getElementById('award')
-let rating = document.getElementById('rating')
+let title = document.getElementById('title');
+let date = document.getElementById('date');
+let plot = document.getElementById('plot');
+let actor = document.getElementById('actors');
+let director = document.getElementById('director');
+let genre = document.getElementById('genre');
+let collection = document.getElementById('collection');
+let poster = document.getElementById('poster');
+let awards = document.getElementById('awards');
+let rating = document.getElementById('ratings');
+let writer = document.getElementById('writer');
 
 
 
 function searchmovie() {
-  let movieinput = document.getElementById("movieinput");
-  let url = api + movieinput.value;
+  let movieInput = document.getElementById("movieInput");
+  let url = api + movieName.value;
 
-  fetch(api)
+  fetch(url )
     .then((data) => {
       return data.json();
     })
     .then((data) => {
-        title.innerText = data.title;
-        data.innerText = data.data;
-        plot.innerText = data.plot;
-        actor.innerText = data.actor;
-        director.innerText = data.director;
-        genre.innerText = data.genre;
-        poster.innerText = data.poster;
-        award.innerText = data.award;
-        collection.innerText = data.collection;
-
+        title.innerText = data.Title;
+        date.innerText = data.Released;
+        plot.innerText = data.Plot;
+        director.innerText = data.Director;
+        actor.innerText = data.Actors;
+        genre.innerText = data.Genre;
+        collection.innerText = data.BoxOffice;
+        rating.innerText = data.imdbRating;
+        writer.innerText = data.Writer;
+        awards.innerText = data.Awards;
+        poster.src = data.Poster;
     });
-  console.log(url);
+  
 }
+
